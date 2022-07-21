115c6569bf7407696f99832b2d4b4c14a4c6d942
Your Name <you@example.com>
2022-07-21 13:45:01 -0300

    ensure HttpAdapter returns null if post returns 204 with data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -23,7 +23,12 @@ class HttpAdapter implements HttpClient {
     };
     final jsonBody = body != null ? jsonEncode(body) : null;
     final response = await client.post(url, headers: headers, body: jsonBody);
-    return response.body.isEmpty ? null : jsonDecode(response.body);
+    if (response.body == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    }
+    else {
+      return null;
+    }
   }
 }
 
@@ -96,6 +101,17 @@ void main() {
 
     test('Should return null if post returns 204', () async {
       mockResponse(200, body: '');
+
+      final response = await sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      expect(response, null);
+    });
+
+    test('Should return null if post returns 204 with data', () async {
+      mockResponse(204);
 
       final response = await sut.request(
         url: url,
