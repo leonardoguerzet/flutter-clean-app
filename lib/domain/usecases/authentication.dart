b98b0b8f7b275606481ef09b46dddef263c1efad
Your Name <you@example.com>
2022-07-20 21:18:33 -0300

    ensure RemoteAuthentication returns an Account on 200

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -14,5 +14,4 @@ class AuthenticationParams {
     @required this.email,
     @required this.secret,
   });
-
 }
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -25,6 +25,11 @@ void main() {
   });
 
   test('Should call HttpClient with correct values', () async {
+    when(httpClient.request(
+        url: anyNamed('url'),
+        method: anyNamed('method'),
+        body: anyNamed('body')))
+        .thenAnswer((_) async => {'acessToken': faker.guid.guid(), 'name': faker.person.name()});
     await sut.auth(params);
 
     verify(httpClient.request(
@@ -79,6 +84,19 @@ void main() {
     final future = sut.auth(params);
 
     expect(future, throwsA(DomainError.invalidCredetials));
+  });
+
+  test('Should return an Account if HttpClient returns 200', () async {
+    final acessToken = faker.guid.guid();
+    when(httpClient.request(
+        url: anyNamed('url'),
+        method: anyNamed('method'),
+        body: anyNamed('body')))
+        .thenAnswer((_) async => {'acessToken': acessToken, 'name': faker.person.name()});
+
+    final account = await sut.auth(params);
+
+    expect(account.token, acessToken);
   });
 
 }
