17dba7ba00fc9ae4bdd810e56000e9e149483667
Your Name <you@example.com>
2022-08-01 22:12:59 -0300

    ensure EmailValidation returns null if email is empty

---
+++ test/validation/validators/email_validation_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_clean_app/validation/protocols/field_validation.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class EmailValidation implements FieldValidation {
+  final String field;
+  EmailValidation(this.field);
+
+  String validate(String value) {
+    return null;
+  }
+}
+
+void main(){
+
+  test('Should return null if email is empty', (){
+    final sut = EmailValidation('any_field');
+
+    final error = sut.validate('');
+
+    expect(error, null);
+  });
+}
+
