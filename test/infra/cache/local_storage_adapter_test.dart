8d9e811739046e52a29a3bb4025d355a1f217776
Your Name <you@example.com>
2022-08-07 13:25:52 -0300

    ensure LocalStorageAdapter calls save secure with correct values

---
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -0,0 +1,34 @@
+import 'package:faker/faker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:meta/meta.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_clean_app/data/cache/cache.dart';
+import 'package:mockito/mockito.dart';
+
+class LocalStorageAdapter implements SaveSecureCacheStorage {
+  final FlutterSecureStorage secureStorage;
+
+  LocalStorageAdapter({@required this.secureStorage});
+
+  Future<void> saveSecure(
+      {@required String key, @required String value}) async {
+    await secureStorage.write(key: key, value: value);
+  }
+}
+
+class FlutterSecureStorageSpy extends Mock implements FlutterSecureStorage {}
+
+void main() {
+  test('Should call save secure with correct values', () async {
+    final secureStorage = FlutterSecureStorageSpy();
+    final sut = LocalStorageAdapter(secureStorage: secureStorage);
+    final key = faker.lorem.word();
+    final value = faker.guid.guid();
+
+    await sut.saveSecure(key: key, value: value);
+
+    verify(secureStorage.write(key: key, value: value));
+  });
+}
