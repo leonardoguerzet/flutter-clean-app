f2da52896df87ecb4f61fb9cb7569bf456785c0b
Your Name <you@example.com>
2022-08-07 13:53:07 -0300

    ensure LocalStorageAdapter throws if save secure throws

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -1,11 +1,11 @@
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:meta/meta.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_clean_app/data/cache/cache.dart';
 import 'package:mockito/mockito.dart';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_clean_app/data/cache/cache.dart';
 
 class LocalStorageAdapter implements SaveSecureCacheStorage {
   final FlutterSecureStorage secureStorage;
@@ -33,10 +33,22 @@ void main() {
     value = faker.guid.guid();
   });
 
+  void mockSaveSecureError() {
+    when(secureStorage.write(key: anyNamed('key'), value: anyNamed('value')))
+        .thenThrow(Exception());
+  }
 
   test('Should call save secure with correct values', () async {
     await sut.saveSecure(key: key, value: value);
 
     verify(secureStorage.write(key: key, value: value));
+  });
+
+  test('Should throw if save secure throws', () async {
+    mockSaveSecureError();
+
+    final future = sut.saveSecure(key: key, value: value);
+
+    expect(future, throwsA(isA<Exception>()));
   });
 }
