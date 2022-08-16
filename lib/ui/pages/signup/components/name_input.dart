671e44284d636244b13827e1d9606476039abab4
Your Name <you@example.com>
2022-08-16 19:45:56 -0300

    ensure SignUpPage calls Validation on any field change

--- lib/ui/pages/signup/components/name_input.dart
+++ lib/ui/pages/signup/components/name_input.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../helpers/helpers.dart';
+import '../signup_presenter.dart';
 
 class NameInput extends StatelessWidget {
   const NameInput({
@@ -9,16 +11,22 @@ class NameInput extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final presenter = Provider.of<SignUpPresenter>(context);
 
-    return TextFormField(
-      decoration: InputDecoration(
-        labelText: R.strings.name,
-        icon: Icon(
-          Icons.person,
-          color: Theme.of(context).primaryColorLight,
-        ),
-      ),
-      keyboardType: TextInputType.name,
-    );
+    return StreamBuilder<UIError>(
+        stream: presenter.nameErrorStream,
+        builder: (context, snapshot) {
+          return TextFormField(
+            decoration: InputDecoration(
+              labelText: R.strings.name,
+              icon: Icon(
+                Icons.person,
+                color: Theme.of(context).primaryColorLight,
+              ),
+            ),
+            keyboardType: TextInputType.name,
+            onChanged: presenter.validateName,
+          );
+        });
   }
 }
--- lib/ui/pages/signup/signup_page.dart
+++ lib/ui/pages/signup/signup_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../components/components.dart';
 import '../../helpers/helpers.dart';
 import 'components/components.dart';
+import 'signup_presenter.dart';
 
 class SignUpPage extends StatelessWidget {
+
+  final SignUpPresenter presenter;
+
+  SignUpPage(this.presenter);
+
   @override
   Widget build(BuildContext context) {
     void _hideKeyboard() {
@@ -27,26 +34,29 @@ class SignUpPage extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.all(32),
-              child: Form(
-                child: Column(
-                  children: <Widget>[
-                    NameInput(),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8),
-                      child: EmailInput(),
-                    ),
-                    PasswordInput(),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 8, bottom: 32),
-                      child: PasswordConfirmationInput(),
-                    ),
-                    SignUpButton(),
-                    FlatButton.icon(
-                      onPressed: () {},
-                      icon: Icon(Icons.exit_to_app),
-                      label: Text(R.strings.login),
-                    ),
-                  ],
+              child: Provider(
+                create: (_) => presenter,
+                child: Form(
+                  child: Column(
+                    children: <Widget>[
+                      NameInput(),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        child: EmailInput(),
+                      ),
+                      PasswordInput(),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8, bottom: 32),
+                        child: PasswordConfirmationInput(),
+                      ),
+                      SignUpButton(),
+                      FlatButton.icon(
+                        onPressed: () {},
+                        icon: Icon(Icons.exit_to_app),
+                        label: Text(R.strings.login),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
