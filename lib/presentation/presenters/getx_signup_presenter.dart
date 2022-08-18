4e6e1ceadd86a44b1b9f0f07ec95169032b189ab
Your Name <you@example.com>
2022-08-18 11:17:07 -0300

    ensure GetxSignUpPresenter emits isFormValid true if all fields are valid

--- lib/presentation/presenters/getx_signup_presenter.dart
+++ lib/presentation/presenters/getx_signup_presenter.dart
@@ -14,10 +14,20 @@ class GetxSignUpPresenter extends GetxController {
   final _passwordConfirmationError = Rx<UIError>();
   final _isFormValid = false.obs;
 
+  String _name;
+  String _email;
+  String _password;
+  String _passwordConfirmation;
+
   Stream<UIError> get emailErrorStream => _emailError.stream;
+
   Stream<UIError> get nameErrorStream => _nameError.stream;
+
   Stream<UIError> get passwordErrorStream => _passwordError.stream;
-  Stream<UIError> get passwordConfirmationErrorStream => _passwordConfirmationError.stream;
+
+  Stream<UIError> get passwordConfirmationErrorStream =>
+      _passwordConfirmationError.stream;
+
   Stream<bool> get isFormValidStream => _isFormValid.stream;
 
   GetxSignUpPresenter({
@@ -25,22 +35,27 @@ class GetxSignUpPresenter extends GetxController {
   });
 
   void validateEmail(String email) {
+    _email = email;
     _emailError.value = _validateField(field: 'email', value: email);
     _validateForm();
   }
 
   void validateName(String name) {
+    _name = name;
     _nameError.value = _validateField(field: 'name', value: name);
     _validateForm();
   }
 
   void validatePassword(String password) {
+    _password = password;
     _passwordError.value = _validateField(field: 'password', value: password);
     _validateForm();
   }
 
   void validatePasswordConfirmation(String passwordConfirmation) {
-    _passwordConfirmationError.value = _validateField(field: 'passwordConfirmation', value: passwordConfirmation);
+    _passwordConfirmation = passwordConfirmation;
+    _passwordConfirmationError.value = _validateField(
+        field: 'passwordConfirmation', value: passwordConfirmation);
     _validateForm();
   }
 
@@ -57,6 +72,13 @@ class GetxSignUpPresenter extends GetxController {
   }
 
   void _validateForm() {
-    _isFormValid.value = false;
+    _isFormValid.value = _emailError.value == null &&
+        _nameError.value == null &&
+        _passwordError.value == null &&
+        _passwordConfirmationError.value == null &&
+        _name != null &&
+        _email != null &&
+        _password != null &&
+        _passwordConfirmation != null;
   }
 }
