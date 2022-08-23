65d7c769b202bb8bd220c27e268b32f13a35460d
Your Name <you@example.com>
2022-08-23 10:53:25 -0300

    ensure MinLengthValidation returns null if value is equal than min size

--- test/validation/validators/min_length_validation_test.dart
+++ test/validation/validators/min_length_validation_test.dart
@@ -12,7 +12,7 @@ class MinLengthValidation implements FieldValidation {
   MinLengthValidation({@required this.field, @required this.size});
 
   ValidationError validate(String value) {
-    return ValidationError.invalidField;
+    return value?.length == size ? null : ValidationError.invalidField;
   }
 }
 
@@ -33,5 +33,9 @@ void main() {
 
   test('Should return error if value is less than min size', () {
     expect(sut.validate(faker.randomGenerator.string(4, min: 1)), ValidationError.invalidField);
+  });
+
+  test('Should return null if value is equal than min size', () {
+    expect(sut.validate(faker.randomGenerator.string(5, min: 5)), null);
   });
 }
