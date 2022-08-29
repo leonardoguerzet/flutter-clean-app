53c9712beaa383038d94e0ce03a52f86c004e535
Your Name <you@example.com>
2022-08-29 17:44:32 -0300

    ensure HttpAdapter throws ServerError if HttpClient throws

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -194,6 +194,10 @@ void main() {
       mockRequest().thenAnswer((_) async => Response(body, statusCode));
     }
 
+    void mockError() {
+      mockRequest().thenThrow(Exception());
+    }
+
     setUp(() {
       mockResponse(200);
     });
@@ -310,6 +314,17 @@ void main() {
 
     test('Should return ServerError if get returns 500', () async {
       mockResponse(500);
+
+      final future = sut.request(
+        url: url,
+        method: 'get',
+      );
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+
+    test('Should return ServerError if get throws', () async {
+      mockError();
 
       final future = sut.request(
         url: url,
