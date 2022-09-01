fdb6aded314aa1a0171d09dbab5671e3a7b444d2
Your Name <you@example.com>
2022-09-01 11:06:40 -0300

    add headers to HttpClient

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -10,48 +10,46 @@ class HttpAdapter implements HttpClient {
 
   HttpAdapter(this.client);
 
-  Future<Map> request({
+  Future<dynamic> request({
     @required String url,
     @required String method,
     Map body,
+    Map headers,
   }) async {
-    final headers = {
-      'content-type': 'application/json',
-      'accept': 'application/json',
-    };
+    final defaultHeaders = headers?.cast<String, String>() ?? {}
+      ..addAll({
+        'content-type': 'application/json',
+        'accept': 'application/json',
+      });
     final jsonBody = body != null ? jsonEncode(body) : null;
     var response = Response('', 500);
     try {
       if (method == 'post') {
-        response = await client.post(url, headers: headers, body: jsonBody);
+        response =
+            await client.post(url, headers: defaultHeaders, body: jsonBody);
       } else if (method == 'get') {
-        response = await client.get(url, headers: headers);
+        response = await client.get(url, headers: defaultHeaders);
       }
-    } catch(error){
+    } catch (error) {
       throw HttpError.serverError;
     }
     return _handleResponse(response);
   }
 
-  Map _handleResponse(Response response) {
+  dynamic _handleResponse(Response response) {
     if (response.statusCode == 200) {
       return response.body.isEmpty ? null : jsonDecode(response.body);
     } else if (response.statusCode == 204) {
       return null;
-    }
-    else if (response.statusCode == 400) {
+    } else if (response.statusCode == 400) {
       throw HttpError.badRequest;
-    }
-    else if (response.statusCode == 401) {
+    } else if (response.statusCode == 401) {
       throw HttpError.unauthorized;
-    }
-    else if (response.statusCode == 403) {
+    } else if (response.statusCode == 403) {
       throw HttpError.forbidden;
-    }
-    else if (response.statusCode == 404) {
+    } else if (response.statusCode == 404) {
       throw HttpError.notFound;
-    }
-    else{
+    } else {
       throw HttpError.serverError;
     }
   }
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -47,11 +47,20 @@ void main() {
     test('Should call post with correct values', () async {
       await sut
           .request(url: url, method: 'post', body: {'any_key': 'any_value'});
-
       verify(client.post(url,
           headers: {
             'content-type': 'application/json',
             'accept': 'application/json',
+          },
+          body: '{"any_key":"any_value"}'));
+
+      await sut
+          .request(url: url, method: 'post', body: {'any_key': 'any_value'}, headers: {'any_header': 'any_value'});
+      verify(client.post(url,
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json',
+            'any_header': 'any_value',
           },
           body: '{"any_key":"any_value"}'));
     });
@@ -203,16 +212,20 @@ void main() {
     });
 
     test('Should call get with correct values', () async {
-      await sut.request(
-        url: url,
-        method: 'get',
-      );
-
-      verify(client.get(
-        url,
+      await sut.request(url: url, method: 'get');
+      verify(client.get(url,
         headers: {
           'content-type': 'application/json',
           'accept': 'application/json'
+        },
+      ));
+
+      await sut.request(url: url, method: 'get', headers: {'any_header': 'any_value'});
+      verify(client.get(url,
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json',
+          'any_header': 'any_value'
         },
       ));
     });
