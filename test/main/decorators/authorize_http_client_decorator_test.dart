af5475484235df899372ce229b3cc4c9fec23079
Your Name <you@example.com>
2022-09-05 10:18:36 -0300

    move AuthorizeHttpClientDecorator to its own file

--- test/main/decorators/authorize_http_client_decorator_test.dart
+++ test/main/decorators/authorize_http_client_decorator_test.dart
@@ -1,40 +1,10 @@
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 
 import 'package:flutter_clean_app/data/cache/cache.dart';
 import 'package:flutter_clean_app/data/http/http.dart';
-
-class AuthorizeHttpClientDecorator implements HttpClient {
-  final FetchSecureCacheStorage fetchSecureCacheStorage;
-  final HttpClient decoratee;
-
-  AuthorizeHttpClientDecorator({
-    @required this.fetchSecureCacheStorage,
-    @required this.decoratee,
-  });
-
-  Future<dynamic> request({
-    @required String url,
-    @required String method,
-    Map body,
-    Map headers,
-  }) async {
-    try {
-      final token = await fetchSecureCacheStorage.fetchSecure('token');
-      final authorizedHeaders = headers ?? {}
-        ..addAll({'x-access-token': token});
-      return await decoratee.request(
-          url: url, method: method, body: body, headers: authorizedHeaders);
-    } on HttpError {
-      rethrow;
-    }
-    catch(error) {
-      throw HttpError.forbidden;
-    }
-  }
-}
+import 'package:flutter_clean_app/main/decorators/decorators.dart';
 
 class FetchSecureCacheStorageSpy extends Mock
     implements FetchSecureCacheStorage {}
