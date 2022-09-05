beaaf313ca9809ab448cfbdf92482f158de06f47
Your Name <you@example.com>
2022-09-05 10:27:01 -0300

    add makeAuthorizeHttpClientDecorator factory

---
+++ lib/main/factories/http/authorize_http_client_decorator_factory.dart
@@ -0,0 +1,11 @@
+import '../../../data/http/http.dart';
+
+import '../../decorators/decorators.dart';
+import '../../factories/factories.dart';
+
+HttpClient makeAuthorizeHttpClientDecorator() {
+  return AuthorizeHttpClientDecorator(
+    decoratee: makeHttpAdapter(),
+    fetchSecureCacheStorage: makeLocalStorageAdapter(),
+  );
+}
