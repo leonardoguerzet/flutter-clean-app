b325afc78c876135f68a330c9d68c4ba73fe48c5
Your Name <you@example.com>
2022-09-06 19:24:19 -0300

    move SUT creation to the setup method

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -2,28 +2,32 @@ import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-
 class LocalLoadSurveys {
   final FetchCacheStorage fetchCacheStorage;
 
   LocalLoadSurveys({@required this.fetchCacheStorage});
 
-   Future<void> load() async {
-     await fetchCacheStorage.fetch('surveys');
+  Future<void> load() async {
+    await fetchCacheStorage.fetch('surveys');
   }
 }
 
-class FetchCacheStorageSpy extends Mock implements FetchCacheStorage{}
+class FetchCacheStorageSpy extends Mock implements FetchCacheStorage {}
 
-abstract class FetchCacheStorage{
+abstract class FetchCacheStorage {
   Future<void> fetch(String key);
 }
 
 void main() {
-  test('Should call FetchCacheStorage with correct key', () async {
-    final fetchCacheStorage = FetchCacheStorageSpy();
-    final sut = LocalLoadSurveys(fetchCacheStorage: fetchCacheStorage);
+  LocalLoadSurveys sut;
+  FetchCacheStorageSpy fetchCacheStorage;
 
+  setUp(() {
+    fetchCacheStorage = FetchCacheStorageSpy();
+    sut = LocalLoadSurveys(fetchCacheStorage: fetchCacheStorage);
+  });
+
+  test('Should call FetchCacheStorage with correct key', () async {
     await sut.load();
 
     verify(fetchCacheStorage.fetch('surveys')).called(1);
