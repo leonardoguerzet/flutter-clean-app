61f2baee97a2569d9153405d742b44c28cd16102
Your Name <you@example.com>
2022-09-11 20:28:01 -0300

    move FetchCacheStorage and LocalLoadSurveys to its own file

---
+++ lib/data/usecases/load_surveys/local_load_surveys.dart
@@ -0,0 +1,27 @@
+import 'package:meta/meta.dart';
+
+import 'package:flutter_clean_app/domain/entities/entities.dart';
+import 'package:flutter_clean_app/domain/helpers/helpers.dart';
+import 'package:flutter_clean_app/data/cache/cache.dart';
+import 'package:flutter_clean_app/data/models/models.dart';
+
+class LocalLoadSurveys {
+  final FetchCacheStorage fetchCacheStorage;
+
+  LocalLoadSurveys({@required this.fetchCacheStorage});
+
+  Future<List<SurveyEntity>> load() async {
+    try {
+      final data = await fetchCacheStorage.fetch('surveys');
+      if (data?.isEmpty != false) {
+        throw Exception();
+      }
+      return data
+          .map<SurveyEntity>(
+              (json) => LocalSurveyModel.fromJson(json).toEntity())
+          .toList();
+    } catch (error) {
+      throw DomainError.unexpected;
+    }
+  }
+}
--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -1,38 +1,15 @@
 import 'package:faker/faker.dart';
-import 'package:flutter_clean_app/data/models/models.dart';
-import 'package:flutter_clean_app/domain/entities/entities.dart';
-import 'package:flutter_clean_app/domain/helpers/helpers.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-class LocalLoadSurveys {
-  final FetchCacheStorage fetchCacheStorage;
+import 'package:flutter_clean_app/domain/entities/entities.dart';
+import 'package:flutter_clean_app/domain/helpers/helpers.dart';
 
-  LocalLoadSurveys({@required this.fetchCacheStorage});
+import 'package:flutter_clean_app/data/cache/cache.dart';
+import 'package:flutter_clean_app/data/usecases/usecases.dart';
 
-  Future<List<SurveyEntity>> load() async {
-    try {
-      final data = await fetchCacheStorage.fetch('surveys');
-      if (data?.isEmpty != false) {
-        throw Exception();
-      }
-      return data
-          .map<SurveyEntity>(
-              (json) => LocalSurveyModel.fromJson(json).toEntity())
-          .toList();
-    } catch (error) {
-      throw DomainError.unexpected;
-    }
-  }
-}
 
 class FetchCacheStorageSpy extends Mock implements FetchCacheStorage {}
-
-abstract class FetchCacheStorage {
-  Future<dynamic> fetch(String key);
-}
-
 
 void main() {
   LocalLoadSurveys sut;
