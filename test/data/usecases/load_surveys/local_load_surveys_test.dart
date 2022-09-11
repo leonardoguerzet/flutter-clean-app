740863d8df3286e18f133e46051044fe46c0a128
Your Name <you@example.com>
2022-09-11 20:16:06 -0300

    ensure LocalLoadSurveys throws UnexpectedError if cache is incomplete

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -117,4 +117,17 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw UnexpectedError if cache is incomplete', () async {
+    mockFetch([
+      {
+        'date': '2022-09-06T00:00:00Z',
+        'didAnswer': 'false',
+      }
+    ]);
+
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
