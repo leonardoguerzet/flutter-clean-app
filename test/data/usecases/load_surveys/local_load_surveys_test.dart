5ca86535f0e466214667d4bf53e2126736e24f7c
Your Name <you@example.com>
2022-09-11 20:05:12 -0300

    ensure LocalLoadSurveys throws UnexpectedError if cache is empty

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -1,6 +1,7 @@
 import 'package:faker/faker.dart';
 import 'package:flutter_clean_app/data/models/models.dart';
 import 'package:flutter_clean_app/domain/entities/entities.dart';
+import 'package:flutter_clean_app/domain/helpers/helpers.dart';
 import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
@@ -12,6 +13,9 @@ class LocalLoadSurveys {
 
   Future<List<SurveyEntity>> load() async {
     final data = await fetchCacheStorage.fetch('surveys');
+    if(data.isEmpty){
+      throw DomainError.unexpected;
+    }
     return data
         .map<SurveyEntity>((json) => LocalSurveyModel.fromJson(json).toEntity())
         .toList();
@@ -77,5 +81,13 @@ void main() {
           dateTime: DateTime.utc(2021, 2, 3),
           didAnswer: true),
     });
+  });
+
+  test('Should throw UnexpectedError if cache is empty', () async {
+    mockFetch([]);
+
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
