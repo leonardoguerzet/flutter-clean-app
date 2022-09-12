e3a13d2adc8879eb47d6c0668c6932e3d7e0b65c
Your Name <you@example.com>
2022-09-12 19:36:07 -0300

    move LocalStorageAdapter to its own file

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -1,20 +1,9 @@
 import 'package:faker/faker.dart';
 import 'package:localstorage/localstorage.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-class LocalStorageAdapter {
-  final LocalStorage localStorage;
-
-  LocalStorageAdapter({@required this.localStorage});
-
-  Future<void> save({@required String key, @required dynamic value}) async {
-    await localStorage.deleteItem(key);
-    await localStorage.setItem(key, value);
-
-  }
-}
+import 'package:flutter_clean_app/infra/cache/cache.dart';
 
 class LocalStorageSpy extends Mock implements LocalStorage {}
 
@@ -24,9 +13,11 @@ void main() {
   String key;
   dynamic value;
 
-  mockDeleteItemError() => when(localStorage.deleteItem(any)).thenThrow(Exception());
+  mockDeleteItemError() =>
+      when(localStorage.deleteItem(any)).thenThrow(Exception());
 
-  mockSetItemError() => when(localStorage.setItem(any, any)).thenThrow(Exception());
+  mockSetItemError() =>
+      when(localStorage.setItem(any, any)).thenThrow(Exception());
 
   setUp(() {
     key = faker.randomGenerator.string(5);
