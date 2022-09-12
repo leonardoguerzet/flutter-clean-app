d36a546198de8d25845ae0ae750673d3fdc00537
Your Name <you@example.com>
2022-09-12 11:43:22 -0300

    ensure LocalLoadSurveys delete cache if it throws

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -149,6 +149,8 @@ void main() {
       mockFetchCall().thenAnswer((_) async => data);
     }
 
+    void mockFetchError() => mockFetchCall().thenThrow(Exception());
+
     setUp(() {
       cacheStorage = CacheStorageSpy();
       sut = LocalLoadSurveys(cacheStorage: cacheStorage);
@@ -183,6 +185,14 @@ void main() {
           'didAnswer': 'false',
         }
       ]);
+
+      await sut.validate();
+
+      verify(cacheStorage.delete('surveys')).called(1);
+    });
+
+    test('Should delete cache if it throws', () async {
+      mockFetchError();
 
       await sut.validate();
 
